dfdc71c8b22f60ad4bac6620271d23280ff9da21
SMAC-Zhang <zhanzhuqiong@163.com>
2025-11-25 13:42:40 +0000

    init lab fs

--- user/user.h
+++ user/user.h
@@ -1,3 +1,6 @@
+#pragma once
+#include "kernel/types.h"
+
 struct stat;
 struct rtcdate;
 
@@ -38,5 +41,6 @@ void* memset(void*, int, uint);
 void* malloc(uint);
 void free(void*);
 int atoi(const char*);
+void itoa(int n, char s[]);
 int memcmp(const void *, const void *, uint);
 void *memcpy(void *, const void *, uint);
